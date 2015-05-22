155a9ca99e11a11939ef8d43e04cfc048140d584
Josh Hagel <josh@14four.com>
2015-05-22 15:05:37 -0700

    Breaking stuff

--- app.js
+++ app.js
@@ -1,50 +1,84 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var Epub = require("epub-gen");
+var path = require('path');
 var app     = express();
 
-app.get('/scrape', function(req, res){
-	// Let's scrape Anchorman 2
-	url = 'http://www.imdb.com/title/tt1229340/';
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get('/scrape', jsonParser, function(req, res){
+	var url = 'https://en.wikipedia.org/wiki/Squirrel';
+	//var url = req.body.url;
+	//console.log(req.body);
 
 	request(url, function(error, response, html){
 		if(!error){
+
 			var $ = cheerio.load(html);
 
-			var title, release, rating;
-			var json = { title : "", release : "", rating : ""};
+			// EPub
+			var file = Date.now() + ".epub";
+			var options = {
+					title: "test",
+					author: "Me", // *Required, name of the author.
+					cover: "http://lorempixel.com/output/abstract-h-c-768-1004-5.jpg",
+					content: [
+							{
+									title: "Chapter 1", // Optional
+									author: "author", // Optional
+									data: "<h1>Learning JavaScript Design Patterns</h1>"
+							},
+							{
+									title: "Chapter 2",
+									data: "<h1>Monkies</h1>"
+							}
+					]
+			};
 
-			$('.header').filter(function(){
-        var data = $(this);
-        title = data.children().first().text();
-        release = data.children().last().children().text();
+			options.title = $('#firstHeading').text();
+			options.publisher = $('#siteSub').text();
+			options.cover = "http:" + $('.infobox img').first().attr('src');
 
-        json.title = title;
-        json.release = release;
-      })
+			new Epub(options, "bin/" + file);
 
-      $('.star-box-giga-star').filter(function(){
-      	var data = $(this);
-      	rating = data.text();
+			res.render('scrape', { title: 'Scrape', message: 'Download your File', file: file});
 
-      	json.rating = rating;
-      })
+		} else {
+
+			res.render('scrape', { title: 'Scrape', message: 'OOPS something went wrong', file: ''});
+
 		}
-
-		fs.writeFile('bin/output.json', JSON.stringify(json, null, 4), function(err){
-    	console.log('File successfully written! - Check your project directory for the output.json file');
-    })
-
-        res.send('Check your console!')
 	})
 })
 
+// Download
+app.get('/download/:id', function(req, res){
+  var file = __dirname + '/bin/'+req.params.id;
+  var filename = path.basename(file);
+
+  res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+  res.setHeader('Content-type', 'application/epub+zip');
+
+  var filestream = fs.createReadStream(file);
+  filestream.pipe(res);
+});
+
 app.get('/', function(req, res){
 
-		res.send('hello world')
+	  res.render('index', { title: 'EPub Generator'});
 
 })
+
+
+app.set('view engine', 'jade');
+
 
 app.listen('8081')
 console.log('Magic happens on port 8081');
